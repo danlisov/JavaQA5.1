faedf133fdb1c6d49905ba290dbe6621cdf2c8a3
Denis Lysov <dan.lysov2015@yandex.ru>
2023-07-04 19:44:13 +0300

    Initial commit

--- src/main/java/ru/netology/javaqa/javaqa5/service/VacationService.java
+++ src/main/java/ru/netology/javaqa/javaqa5/service/VacationService.java
@@ -2,11 +2,11 @@ package ru.netology.javaqa.javaqa5.service;
 public class VacationService {
 
     public int calculate(int income, int expenses, int threshold) {
-        int count = 0; // счётчик месяцев отдыха
-        int money = 0; // количество денег на счету
+        int count = 0;
+        int money = 0;
         for (int month = 0; month < 12; month++) {
-            if (money >= threshold) { // можем ли отдыхать?
-                count++; // увеличиваем счётчик месяцев отдыха
+            if (money >= threshold) {
+                count++;
                 money = (money - expenses) /3;
             } else {
                 money = money + income;
